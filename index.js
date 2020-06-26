130d398ef5dcb0c7cf1b84e8d1a8f706d7475d5d
Amit Kinor <amit.kinor@gmail.com>
2020-06-26 09:50:34 +0300

    stable

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -9,6 +10,9 @@ const courses = [
     {  id: 3 , name: 'course3' }
 ];
 
+
+//////  getters
+
 app.get('/', (req,res) => {
     res.send('Hello Kinor!!');
 });
@@ -17,17 +21,19 @@ app.get('/api/courses', (req,res) => {
     res.send(courses);
 });
 
+
+
 app.get('/api/courses/:id', (req,res)=> {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send('The id was not found');
+    if (!course) return res.status(404).send('The id was not found');
     res.send(course);
 });
 
+
+/////////   Post
 app.post('/api/courses', (req, res) =>{
-    if (!req.body.name || req.body.name.length < 3){
-        req.status(400).send('some rules....')
-        return;
-    }
+    const { error } = validateCourse(req.body);
+    if (error)     return res.status(400).send(error.details[0].message);
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -35,6 +41,45 @@ app.post('/api/courses', (req, res) =>{
     courses.push(course);
     res.send(course);
 });
+
+////////    put
+
+app.put('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('The id was not found');
+       
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+           
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+//////  detele
+
+app.delete('/api/courses/:id', (req,res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The id was not found');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    console.log('bummer');
+
+    res.send(course);
+});
+    
+
+/////////  utils  ////////////////
+
+
+function validateCourse(course){
+    const schema = {
+        name:Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 // PORT
 const port = process.env.PORT || 3000;
